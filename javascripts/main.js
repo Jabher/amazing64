8d74cd2f3b8b20d7579e3a97e6602f137488bcc2
Vsevolod Rodionov <crbrus@gmail.com>
2014-01-07 17:40:52 +0400

    analytics

--- javascripts/main.js
+++ javascripts/main.js
@@ -241,5 +241,13 @@ var viewManager = (function () {
 }).call(this);
 
 
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-46900286-1', 'jabher.github.io');
+ga('send', 'pageview');
+
 //todo dragndrop элементов
 //todo кнопка сброса
