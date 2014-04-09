ac2fb0509d26184efd92278dbcc477dfc6caf8ab
Vsevolod Rodionov <crbrus@gmail.com>
2014-04-09 13:14:52 +0400

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -104,7 +104,7 @@ var viewManager = (function () {
             return '<span class="imageSize">' + dataSet.width + 'px X ' + dataSet.height + 'px</span>'
         },
         colorDesc: function (dataSet) {
-            if (dataSet.color) return '';
+            if (!dataSet.color) return '';
             console.log(((dataSet.color.reduce(function(a,b){return a+b}) < 512) ? 'color: white' : ''));
             return '<span class="mainColor" style="' +
                 '/*noinspection CssRgbFunction*/ background: rgb(' + dataSet.color.join(',') + ');' +
